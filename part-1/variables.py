fc68f8296f56cf8b2be600301ed301bcbec074ef
edjohn00 <johneddieablao99@gmail.com>
2023-02-13 17:27:25 +0800

    add a variable learning in python

---
+++ part-1/variables.py
@@ -0,0 +1,13 @@
+# VARIABLE NAME
+name = 'anna'
+
+print(name + ' is a girl')
+print(name + ' is 18')
+print(name + ' is from USA')
+print(name)
+
+# ASSIGN Multiple Variables
+name, age, location = 'anna', 18, 'USA'
+print(name + ' is is a girl')
+print(name + ' is ' , age)
+print(name + ' is from ' + location)
