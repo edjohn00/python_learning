e577201b1568b37a0ba502aaa79721b27c1b3d7b
edjohn00 <johneddieablao99@gmail.com>
2023-02-20 21:13:35 +0800

    adding learning abt user's input

---
+++ part-1/users_input.py
@@ -0,0 +1,12 @@
+#in this part we are able to ask the user for input, by uses the input() method
+
+#name = input("Enter your Name: ")
+#age = input("Enter your Age: ")
+#print("Your name is " + name + " and your age is " + age + "years old")
+
+#simle word replacement replace() method replaces a specified phrase with another specified phrase
+sentence = input("Enter a sentence: ")
+print("Type a sentence: " + sentence)
+word_replace = input("Enter you word that you to replace in the sentence: ")
+word_replace_with = input("Enter the word replace with: ")
+print(sentence.replace(word_replace, word_replace_with))
