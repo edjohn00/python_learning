33ebbba848bd9133cbe112924fd6d027a922310c
edjohn00 <johneddieablao99@gmail.com>
2023-02-22 01:39:06 +0800

    add list methods

---
+++ part-1/list_methods.py
@@ -0,0 +1,5 @@
+#first method in list is extend() --- add the elements of a list (or any iterable), to the end of the current list
+list_num = [0, 1, 2, 3, 4, 5, 6]
+list_fruits = ["apple", "banana", "manggo", "Cherry"]
+list_num.extend(list_fruits)
+print(list_num)
