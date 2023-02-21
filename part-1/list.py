8d40bdf7e7b04ebcf354a540d070f6fc53e2ed0d
edjohn00 <johneddieablao99@gmail.com>
2023-02-21 18:37:44 +0800

    learning list in python

---
+++ part-1/list.py
@@ -0,0 +1,33 @@
+#Lists are used to store multiple items in a single variable.
+#Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+#Lists are created using square brackets:
+
+countries = ["USA", "Japan", "Philippines", "Korea", "Taiwan", "Canada"]
+print(countries)
+
+#always remember in python or programming in general numbers are counting from zero 
+#if you want to print one by one like you want to print "Japan" all you need to do is use square brackets then insert the index number like 1
+print(countries[1])
+
+#this is how to get the index value of japan insert another square bracket then add the number
+print(countries[1][0]) 
+
+#if you want to get the index from 1 - 4 or Japan - Canada this is how works
+print(countries[1:])
+#if you use negative sign it will print the last value in the list
+print(countries[-1])
+
+#if you want to check the type of the particular variable insert "type" word to print function
+print(type(countries))
+
+#also you can change the particular value of the list 
+countries[0] = "United Kingdom"
+countries[5] = "Australia"
+print(countries)
+
+#you want to see how many index value in the variable you you need is to add "len" in the print
+print(len(countries))
+
+#also in list we can mix defferent data type in the list
+countries2 = [1, "USA", "Japan", 10, "Korea", 5, "Canada", True]
+print(countries2)
