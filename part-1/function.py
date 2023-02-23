4ab677a98886fa80896f6258f564bc015786cd80
edjohn00 <johneddieablao99@gmail.com>
2023-02-23 20:02:41 +0800

    adding learning function in python

---
+++ part-1/function.py
@@ -0,0 +1,6 @@
+#Function is a block of code with only runs when it's called also can return dtas as a results
+#in python a function is defined using the def keyword
+def function(name):
+    print("Hi it's me I'm the problem it me" + name)
+function("john eddie")
+ 
---
+++ part-1/turples.py
@@ -0,0 +1,8 @@
+#Tuples are used to store multiple items in a single variable
+#Tuples are very similar to list, but turples are immutable means you can't change any value or unchangeable
+#Tuples written with round brackets
+
+numbers = (2, 4, 6, 8, 10, 12)
+print(numbers)
+
+ 
