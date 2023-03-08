e9451c53e18ee1144e1d73418221fa179cd73524
edjohn00 <johneddieablao99@gmail.com>
2023-03-08 16:58:33 +0800

    start learning dictionaries in python

---
+++ excercises/checker_program.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: ")) #calculate if number is even or odd
+
+if num %2 == 0:
+    print(num, "this is even number")
+else:
+    print(num, "this is odd number")
---
+++ part-1/dictionaries.py
@@ -0,0 +1,3 @@
+#Dictionaries are used to store data values in key:value pairs, 
+#is a collection which is ordered*, changeable and do not allow duplicates.
+#
