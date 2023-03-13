1d54c3640b82ffca3d70af365d9303da0e0dd511
edjohn00 <johneddieablao99@gmail.com>
2023-03-14 07:51:16 +0800

    add exercises about getting users information

---
+++ excercises/getting_user_info.py
@@ -0,0 +1,14 @@
+numRegistration = int(input("Enter your number line: "))
+print("You are in the list!!")
+print("Enter your personal information to start register to our system \n")
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+address = input("Enter your address: ")
+number = int(input("Enter your number: "))
+
+print("\nThis is your personal information check it: ")
+print("Name -" + firstName +  lastName + "\nAge - " + str(age) + "\nGenter - " + gender)
+print("Address - " + address + "\nNumber - " + str(number))
