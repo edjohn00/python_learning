fa83c6fb245b72b4d1f987f21c4fdc912c8edce1
edjohn00 <johneddieablao99@gmail.com>
2023-04-04 13:35:13 +0800

    done learning in conditional statement

--- learning/tutorial5.py
+++ learning/tutorial5.py
@@ -24,6 +24,10 @@
     if valueOne > valueTwo                          if age >= 18:
         #do something                                   print("Legal Age")
 """
+# age = int(input("Enter your Age: "))
+
+# if age >= 18:
+#     print("Legal Age")
 
 """
     IF-ELSE STATEMENT - used when dealing with Two Conditions
@@ -33,6 +37,12 @@
     else:                                           else:
         #do something                                   print("Too Young")
 """
+# number = int(input("Enter a number: "))
+
+# if number > 10:
+#     print("number is greater than 10")
+# else:
+#     print("number is less than 10")
 
 """
     IF-ELIF-ELSE STATEMENT - used when dealing with THREE OR MORE Conditions
@@ -44,6 +54,14 @@
     else:                                           else:
         #do something                                   print("Too Young")
 """
+# age = int(input("Enter a number: "))
+
+# if age >= 18:
+#     print("Legal Age")
+# elif age >= 13:
+#     print("Teenage")
+# else: 
+#     print("Too young")
 
 """
     NESRED CONDITIONAL STATEMENT - used when dealing with CONDITIONS INSIDE A CONDITION
@@ -56,12 +74,46 @@
         else:                                           else:
             #do something                                   print("Legal Age and Short")
 """
+# age = int(input("Enter Age: "))
+# height = int(input("Enter Height: "))
+
+# if age >= 18: 
+#     if height >= 176:
+#         print("Tall and Legal Age")
+#     elif height >= 156:
+#         print("Legal Age and Short")
+# else:
+#     print("Too Young")
+
+"""
+    NOT KEYWORD - used to invert the condition value
+    syntax:                                         Example:
+    if not valueOne == valueTwo:                    if not age >= 18:
+        #do something                                   print("Too young")
+"""
+# age = int(input("Enter Age: "))
+
+# if not age >= 18:
+#     print("Too Young")
+# else:
+#     print("Legal age")
 
 """
     LOGICAL OPERATORS - used to include 2 or MORE Condetion in one line 
     * AND - BOTH condition must be true
     * OR - EITHER condition must be true 
 """
+# age = int(input("Enter Age: "))
+# height = int(input("Enter Height: "))
+
+# if age >= 18 and height >= 176:
+#     print("Tall and Legal Age")
+# elif age >= height >= 150:
+#     print("Average and Legal Age")
+# elif age >= 18:
+#     print("Short and Legal Age")
+# else:
+#     print("Too Young")
 
 """
     COLLECTION CONDITIONAL STATEMENTS - used to check an item if it's in a collection (list and tuples)
@@ -72,3 +124,10 @@
     else:                                           else:
         #do something                                   print("All Clear")
 """
+
+bag = ["wallet", "gun", "chocolate", "knife", "bottle water"]
+
+if "gun" and "knife" in bag:
+    print("Your not allowed to enter")
+else:
+    print("you are free to enter")
