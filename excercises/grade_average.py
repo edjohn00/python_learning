71a9ebdce72dfa858097f48f8aca6cd56f0f570e
edjohn00 <johneddieablao99@gmail.com>
2023-04-05 08:21:48 +0800

    add excercises in conditional statement

--- excercises/grade_average.py
+++ excercises/grade_average.py
@@ -10,3 +10,24 @@
 
     PS: Print the necessary message for each condition
 """
+Filipino = float(input("Enter a grade in Filipino: "))
+English = float(input("Enter a grade in English: "))
+Math = float(input("Enter a grade in Math: "))
+Science = float(input("Enter a grade in Science: "))
+Programming = float(input("Enter a grade Programming: "))
+
+student_average = (Filipino + English + Math + Science + Programming) / 5
+
+if student_average > 100 or student_average <= 50:
+    print("Your grades is invalid")
+elif student_average >= 98:
+    print("Your With Highest Honor")
+    print("Congratulation")
+elif student_average >= 95:
+    print("Your With High Honor")
+elif student_average >= 90:
+    print("Your With Honor")
+elif student_average >= 75:
+    print("Your Passed, Study more to get a higher grade")
+else:
+    print("You failed this semister")
