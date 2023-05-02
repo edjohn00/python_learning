c492474ba215c2583812151c14161dba008e4792
edjohn00 <johneddieablao99@gmail.com>
2023-05-02 17:33:29 +0800

    done with lambda tutorial

---
+++ learning/tutorial11.py
@@ -0,0 +1,13 @@
+# LAMBDA | SHORTHAND FUNCTIONED
+
+"""
+    Lambda - is a small Anonymous Function that can take any amount of Arguments but can only have one Expression
+    
+    Creating Lambda Function 
+    Syntax:
+        lambda p1, p2: expression
+"""
+
+add = lambda num1, num2: num1 + num2
+
+print(add(23,2))
