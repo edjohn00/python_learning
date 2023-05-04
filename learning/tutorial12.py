c4bfa9b8bc42debcd1ff8b657e947c1d1418cde7
edjohn00 <johneddieablao99@gmail.com>
2023-05-04 18:30:05 +0800

    add notes abt OOP

---
+++ learning/tutorial12.py
@@ -0,0 +1,56 @@
+#CLASSES & OBJECT 
+"""
+    Onject Oriented Programming - it aims to implement real world object / entities its attributes and behavior into programming so that we can replsent it in our game, software or applications.
+
+    Objects - anything that has an attribute and a purpose is an object.
+        example:
+        * Game Characters
+        * Real Life People (Student, Employees)
+        * Real Life Objects (Fruits, Table)
+
+    Class - used in programming to make a blueprint for our object. It will represent their attribute and purpose
+        example:
+            * Game Characters
+                * Purpose
+                    * Physical Attack
+                    * Magic Attack
+                * Attributes
+                    * NAME
+                    * HP
+                    * MP
+    
+    Creating Classes
+        Syntax:
+
+        class className:
+            #attributes
+            #purpose/functions
+"""
+
+
+
+"""
+    Creating Classes
+        Syntax:
+
+        class className:
+            #attributes
+            #purpose/functions
+
+        Identifier = className()
+"""
+
+
+"""
+    Creating Classes
+        Syntax:
+
+        class className:
+            #attributes
+            #purpose/functions
+
+        Identifier = className()
+
+        Identifier.attribute = value
+        print(Identifier.attribute) 
+"""
